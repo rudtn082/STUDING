bdc5386e430dbf541089fd6bb050cd1653cfcee0
rudtn082 <rudtn082>
2019-03-29 20:03:24 +0900

    1000, 1001, 2558, 10950, 10951 add

---
+++ Q_1000/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+
+        int A = SC.nextInt();
+        int B = SC.nextInt();
+
+        if(A <= 0 && B >= 10) return;
+
+        System.out.println(A+B);
+    }
+}
