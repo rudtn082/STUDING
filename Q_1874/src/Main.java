567ce72c4af6b516baabbdd175dbd2777d960f24
rudtn082 <rudtn082@naver.com>
2019-04-04 01:00:12 +0900

    add Q_1874

--- Q_1874/src/Main.java
+++ Q_1874/src/Main.java
@@ -6,17 +6,42 @@ public class Main {
         Scanner SC = new Scanner(System.in);
         int num = SC.nextInt();
 
-        int[] array_1 = new int[num]; // 1~num까지의 배열
-        int[] array_2 = new int[num]; // 완성해야할 배열
+        ArrayList<Integer> stack = new ArrayList<>();
+        int[] array_2 = new int[num]; // 입력된 수열
 
         for(int i = 0; i < num; i++) { // 배열 채워넣기
-            array_1[i] = i+1;
             array_2[i] = SC.nextInt();
         }
 
+        ArrayList<Character> printArray = new ArrayList<>();
 
+        int k = 0, i = 1;
+        while(k != num) {
+            int searchNum = array_2[k];
+            while (true) {
+                    if(stack.size() == 0 || stack.get(stack.size()-1) != searchNum) {
+                        stack.add(i++);
+                        printArray.add('+');
+                        if(i > num+1) {
+                            System.out.println("NO");
+                            return;
+                        }
+                    }
+                    else {
+                        if(stack.size() <= 0) {
+                            System.out.println("NO");
+                            return;
+                        }
+                        stack.remove(stack.size()-1);
+                        printArray.add('-');
+                        break;
+                    }
+            }
+            k++;
+        }
 
-
-
+        for(int j = 0; j < printArray.size(); j++) {
+            System.out.println(printArray.get(j));
+        }
     }
 }
